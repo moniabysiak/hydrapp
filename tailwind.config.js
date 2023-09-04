5a5a8100f7ea529d5e5f4f83615d43365a5ea10a
Monika Bysiak <monika.serwin88@gmail.com>
2023-09-04 21:17:19 +0200

    transfer the appearance to the code

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "page":"#0F172A"
+      },
+    },
   },
   plugins: [],
 }
