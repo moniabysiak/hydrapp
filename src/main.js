dbec19f0c1436d12f5019c78b20103846be781bf
Monika Bysiak <monika.serwin88@gmail.com>
2023-09-05 19:16:56 +0200

    added querySelector

---
+++ src/main.js
@@ -0,0 +1,8 @@
+const addGlass = document.querySelector('.add-glass--js');
+console.log(addGlass)
+
+const deleteGlass = document.querySelector('.delete-glass--js');
+console.log(deleteGlass)
+
+const counter = document.querySelector('.counter--js');
+console.log(counter)
