b0c0a6b44ce1c07c45f04c33b2b6ade7e4c48f16
Monika Bysiak <monika.serwin88@gmail.com>
2023-09-05 20:33:26 +0200

    wrong js

--- src/main.js
+++ src/main.js
@@ -6,3 +6,22 @@ console.log(deleteGlass)
 
 const counter = document.querySelector('.counter--js');
 console.log(counter)
+
+var numberOfGlasses = 0;
+
+addGlass.addEventListener("click", function() { 
+    numberOfGlasses += 1;
+    counter.innerHTML = numberOfGlasses; 
+  })
+
+
+
+
+// var numberOfGlasses = document.getElementById(counter),
+//   count = 0;
+//   numberOfGlasses.onclick = function() {
+//   count += 1;
+//   numberOfGlasses.innerHTML = count;
+// }
+
+
