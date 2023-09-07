04ccbe031f5c2a6c1a7ff9c28a3e18b307258e00
Monika Bysiak <monika.serwin88@gmail.com>
2023-09-07 12:25:03 +0200

    when the key is empty put 0

--- src/main.js
+++ src/main.js
@@ -1,26 +1,27 @@
 const addGlass = document.querySelector('.add--glass--js');
-console.log(addGlass)
-
 const deleteGlass = document.querySelector('.delete--glass--js');
-console.log(deleteGlass)
-
 const counter = document.querySelector('.counter--js');
-console.log(counter)
 
 const key = new Date().toISOString().slice(0, 10)
 var numberOfGlasses = 0;
 
-addGlass.addEventListener("click", function() { 
-    numberOfGlasses += 1;
-    counter.innerHTML = numberOfGlasses; 
-    localStorage.setItem(key, numberOfGlasses);
-  }
+addGlass.addEventListener("click", function () {
+  numberOfGlasses += 1;
+  counter.innerHTML = numberOfGlasses;
+  localStorage.setItem(key, numberOfGlasses);
+}
 )
 
-deleteGlass.addEventListener("click", function() {
-  if (numberOfGlasses >0) {
+deleteGlass.addEventListener("click", function () {
+  if (numberOfGlasses > 0) {
     numberOfGlasses -= 1;
   }
-  counter.innerHTML = numberOfGlasses; 
+  counter.innerHTML = numberOfGlasses;
   localStorage.setItem(key, numberOfGlasses);
 })
+
+let checkGlasses = localStorage.getItem(parseInt(numberOfGlasses));
+
+if (!checkGlasses) {
+  localStorage.setItem(key, 0);
+}
