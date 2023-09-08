146e4da81398ef2777ca22c7ff065dd7c0a186ae
Monika Bysiak <monika.serwin88@gmail.com>
2023-09-08 11:48:21 +0200

    back to version with checkGlasses

--- src/main.js
+++ src/main.js
@@ -4,15 +4,6 @@ const counter = document.querySelector('.counter--js');
 
 const key = new Date().toISOString().slice(0, 10)
 var numberOfGlasses = 0;
-var numberOfGlassesFromLocalStorage = localStorage.getItem(key);
-
-if (numberOfGlassesFromLocalStorage == null) {
-  localStorage.setItem(key, 0);
-  counter.innerHTML = 0;
-} else {
-  numberOfGlasses = parseInt(numberOfGlassesFromLocalStorage);
-  counter.innerHTML = numberOfGlasses;
-}
 
 addGlass.addEventListener("click", function () {
   numberOfGlasses += 1;
@@ -29,3 +20,12 @@ deleteGlass.addEventListener("click", function () {
   localStorage.setItem(key, numberOfGlasses);
 })
 
+let checkGlasses = parseInt(localStorage.getItem(key));
+console.log(checkGlasses);
+
+if (checkGlasses == null) {
+  localStorage.setItem(key, 0);
+} else {
+  numberOfGlasses = checkGlasses;
+  counter.innerHTML = checkGlasses;
+}
