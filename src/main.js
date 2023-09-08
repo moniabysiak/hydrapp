02076041cb0109439098fe1d890809414284000f
Monika Bysiak <monika.serwin88@gmail.com>
2023-09-08 11:42:33 +0200

    fix Nan error

--- src/main.js
+++ src/main.js
@@ -3,7 +3,16 @@ const deleteGlass = document.querySelector('.delete--glass--js');
 const counter = document.querySelector('.counter--js');
 
 const key = new Date().toISOString().slice(0, 10)
-var numberOfGlasses = parseInt(localStorage.getItem(key));
+var numberOfGlasses = 0;
+var numberOfGlassesFromLocalStorage = localStorage.getItem(key);
+
+if (numberOfGlassesFromLocalStorage == null) {
+  localStorage.setItem(key, 0);
+  counter.innerHTML = 0;
+} else {
+  numberOfGlasses = parseInt(numberOfGlassesFromLocalStorage);
+  counter.innerHTML = numberOfGlasses;
+}
 
 addGlass.addEventListener("click", function () {
   numberOfGlasses += 1;
@@ -20,10 +29,3 @@ deleteGlass.addEventListener("click", function () {
   localStorage.setItem(key, numberOfGlasses);
 })
 
-if (numberOfGlasses == null) {
-    localStorage.setItem(key, 0);
-    counter.innerHTML = 0;
-    numberOfGlasses = 0;
-  } else {
-    counter.innerHTML = numberOfGlasses;
-  }
