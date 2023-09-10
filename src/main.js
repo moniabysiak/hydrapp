43450f47136dfbd945c372238f097f7a19c66454
Monika Bysiak <monika.serwin88@gmail.com>
2023-09-10 15:20:56 +0200

    changed with MK homework

--- src/main.js
+++ src/main.js
@@ -3,18 +3,17 @@ const deleteGlass = document.querySelector('.delete--glass--js');
 const counter = document.querySelector('.counter--js');
 
 const key = new Date().toISOString().slice(0, 10)
-var numberOfGlasses = 0;
+let numberOfGlasses = 0;
 
 let checkGlasses = localStorage.getItem(key);
 console.log(checkGlasses);
 
 if (checkGlasses == null) {
   localStorage.setItem(key, 0);
-  counter.innerHTML = numberOfGlasses;
 } else {
   numberOfGlasses = parseInt(checkGlasses);
-  counter.innerHTML = numberOfGlasses;
 }
+counter.innerHTML = numberOfGlasses;
 
 addGlass.addEventListener("click", function () {
   numberOfGlasses += 1;
